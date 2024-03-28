cf29f4ec482634baf397560839a9db00cc6c4281
aktan <76080041+raiym2048@users.noreply.github.com>
2024-03-28 18:04:16 +0600

    Initial commit

---
+++ src/main/java/kg/it_lab/backend/Osh/OshApplication.java
@@ -0,0 +1,13 @@
+package kg.it_lab.backend.Osh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/it_lab/backend/Osh/OshApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.it_lab.backend.Osh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
