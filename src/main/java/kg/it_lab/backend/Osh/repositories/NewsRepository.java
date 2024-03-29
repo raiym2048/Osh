c4c05c498f3b4e0da61e456371e698bee9f4d349
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2024-03-29 16:22:14 +0600

    intital structure_2

---
+++ src/main/java/kg/it_lab/backend/Osh/repositories/NewsRepository.java
@@ -0,0 +1,9 @@
+package kg.it_lab.backend.Osh.repositories;
+
+import kg.it_lab.backend.Osh.entities.News;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface NewsRepository extends JpaRepository<News, Long> {
+}
---
+++ src/main/java/kg/it_lab/backend/Osh/services/impl/NewsServiceImpl.java
@@ -0,0 +1,8 @@
+package kg.it_lab.backend.Osh.services.impl;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NewsServiceImpl {
+}
