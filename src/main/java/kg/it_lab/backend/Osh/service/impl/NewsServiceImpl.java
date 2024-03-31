cb0c3ad0b2f7954ebb7d4b187b19eacee90ce3bf
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2024-04-01 01:14:57 +0600

    news getAll/getById without images

--- src/main/java/kg/it_lab/backend/Osh/service/impl/NewsServiceImpl.java
+++ src/main/java/kg/it_lab/backend/Osh/service/impl/NewsServiceImpl.java
@@ -1,13 +1,36 @@
 package kg.it_lab.backend.Osh.service.impl;
+import kg.it_lab.backend.Osh.dto.news.NewsDetailResponse;
+import kg.it_lab.backend.Osh.dto.news.NewsResponse;
+import kg.it_lab.backend.Osh.entities.News;
+import kg.it_lab.backend.Osh.exception.NotFoundException;
+import kg.it_lab.backend.Osh.mapper.NewsMapper;
+import kg.it_lab.backend.Osh.repository.NewsRepository;
 import kg.it_lab.backend.Osh.service.NewsService;
 import lombok.AllArgsConstructor;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
 
 
 @Service
 @AllArgsConstructor
 public class NewsServiceImpl implements NewsService {
+    private final NewsMapper newsMapper;
+    private final NewsRepository newsRepository;
 
+    @Override
+    public List<NewsResponse> all() {
+        return newsMapper.toDtos(newsRepository.findAll());
+    }
+
+    @Override
+    public NewsDetailResponse detail(Long id) {
+        Optional<News> news = newsRepository.findById(id);
+        if(news.isEmpty())
+            throw new NotFoundException("News with id: " + id + " - not found!", HttpStatus.NOT_FOUND);
+        return newsMapper.toDetailDto(news.get());
+    }
 }
