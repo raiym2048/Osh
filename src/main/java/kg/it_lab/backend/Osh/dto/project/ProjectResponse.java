f795852f221846b08c3b41e3ea574431f4132f79
Marlen Ormonbaev <marlen.ormonbaev05@gmail.com>
2024-04-04 00:44:59 +0600

    project mapper done

--- src/main/java/kg/it_lab/backend/Osh/dto/project/ProjectResponse.java
+++ src/main/java/kg/it_lab/backend/Osh/dto/project/ProjectResponse.java
@@ -3,11 +3,13 @@ package kg.it_lab.backend.Osh.dto.project;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class ProjectResponse {
     private Long id;
-    private String imagePath;
+    private List<String> imagePaths;
     private String description;
     private String name;
     private String subtopic;
